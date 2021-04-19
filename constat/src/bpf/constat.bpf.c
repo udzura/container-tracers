f5c9ca5ffe405738adaaea299c3b237c89769c23
Uchio Kondo <udzura@udzura.jp>
2021-04-19 20:03:28 +0900

    Initial stat

---
+++ constat/src/bpf/constat.bpf.c
@@ -0,0 +1,65 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+
+struct key_t {
+  u32 tid;
+  u64 syscall_nr;
+};
+struct leaf_t{
+  u64 count;
+  u64 elapsed_ns;
+  u64 enter_ns;
+};
+
+struct {
+  __uint(type, BPF_MAP_TYPE_HASH);
+  __uint(max_entries, 30000);
+  __type(key, struct key_t);
+  __type(value, struct leaf_t);
+} dist SEC(".maps");
+
+SEC("tracepoint/raw_syscalls/sys_enter")
+int tracepoint__raw_syscalls__sys_enter(struct trace_event_raw_sys_enter* ctx)
+{
+  struct key_t key = {0};
+  struct leaf_t initial = {0}, val, *val_;
+  key.tid = bpf_get_current_pid_tgid();
+  key.syscall_nr = ctx->id;
+
+  val_ = bpf_map_lookup_elem(&dist, &key);
+  if (!val_) {
+    bpf_map_update_elem(&dist, &key, &initial, 0);
+    val_ = bpf_map_lookup_elem(&dist, &key);
+    if (!val_)
+      goto cleanup;
+  }
+
+  val = *val_;
+  val.count += 1;
+  val.enter_ns = bpf_ktime_get_ns();
+  bpf_map_update_elem(&dist, &key, &val, 2);
+
+cleanup:
+  return 0;
+}
+
+SEC("tracepoint/raw_syscalls/sys_exit")
+int tracepoint__raw_syscalls__sys_exit(struct trace_event_raw_sys_exit* ctx)
+{
+  struct key_t key = {0};
+  struct leaf_t val, *val_;
+  key.tid = bpf_get_current_pid_tgid();
+  key.syscall_nr = ctx->id;
+
+  val_ = bpf_map_lookup_elem(&dist, &key);
+  if (val_) {
+    val = *val_;
+    u64 delta = bpf_ktime_get_ns() - val.enter_ns;
+    val.enter_ns = 0;
+    val.elapsed_ns += delta;
+
+    bpf_map_update_elem(&dist, &key, &val, 2);
+  }
+  return 0;
+}
--- constat/src/main.rs
+++ constat/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use libbpf_rs::MapFlags;
+use plain::Plain;
+
+mod bpf;
+use bpf::*;
+
+#[repr(C)]
+#[derive(Default, Debug)]
+struct Key {
+    pub tid: u32,
+    pub syscall_nr: u64,
+}
+unsafe impl Plain for Key {}
+
+#[repr(C)]
+#[derive(Default, Debug)]
+struct Value {
+    pub count: u64,
+    pub elapsed_ns: u64,
+    pub enter_ns: u64,
+}
+unsafe impl Plain for Value {}
+
+fn main() -> Result<()> {
+    let mut skel_builder: ConstatSkelBuilder = ConstatSkelBuilder::default();
+    let mut open_skel: OpenConstatSkel = skel_builder.open()?;
+    // open_skel.rodata()...
+
+    let mut skel: ConstatSkel = open_skel.load()?;
+    skel.attach()?;
+
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    let mut maps = skel.maps();
+    let mut dist = maps.dist();
+    for key in dist.keys() {
+        if let Some(value) = dist.lookup(&key, MapFlags::empty())? {
+            let mut key_ = Key::default();
+            plain::copy_from_bytes(&mut key_, &key).expect("invalid key bytes");
+            let mut value_ = Value::default();
+            plain::copy_from_bytes(&mut value_, &value).expect("invalid value bytes");
+            println!("key={:?}, value={:?}", key_, value_);
+        }
+    }
+
+    Ok(())
 }
