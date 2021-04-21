eb30fe65212bd36466973c082d2837aafa57e23f
Uchio Kondo <udzura@udzura.jp>
2021-04-21 19:06:01 +0900

    Options

--- conbiograph/src/main.rs
+++ conbiograph/src/main.rs
@@ -2,16 +2,29 @@ use anyhow::Result;
 use libbpf_rs::MapFlags;
 use plain::Plain;
 use structopt::StructOpt;
+use textplots::{Chart, Plot, Shape};
 
-use std::collections::HashMap;
+use std::io::{BufWriter, Write};
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread::sleep;
 use std::time::Duration;
-use std::{mem, slice};
 
 mod bpf;
 use bpf::*;
+
+#[derive(Debug, StructOpt)]
+struct Command {
+    /// Show operation count instead of bytesize
+    #[structopt(long = "count")]
+    show_count: bool,
+    /// Show avarage operation bytesize per period
+    #[structopt(long = "avg")]
+    show_avg: bool,
+    /// Summrization period (ms)
+    #[structopt(short = "t", long = "period", default_value = "1000")]
+    period: u64,
+}
 
 #[repr(C)]
 #[derive(Default, Debug)]
@@ -29,8 +42,10 @@ struct Value {
 unsafe impl Plain for Value {}
 
 fn main() -> Result<()> {
+    let opts: Command = Command::from_args();
+
     let skel_builder: ConbiographSkelBuilder = ConbiographSkelBuilder::default();
-    let mut open_skel = skel_builder.open()?;
+    let open_skel = skel_builder.open()?;
 
     let mut skel = open_skel.load()?;
     skel.attach()?;
@@ -42,9 +57,15 @@ fn main() -> Result<()> {
         r.store(false, Ordering::SeqCst);
     })?;
 
-    println!("Collecting events... Ctrl-C to stop and show stat");
+    let mut points: Vec<(f32, f32)> = Vec::new();
+    let mut countup = 0;
+
+    println!(
+        "Collecting events in {} ms... Ctrl-C to stop and show stat",
+        opts.period
+    );
     while running.load(Ordering::SeqCst) {
-        sleep(Duration::from_secs(1));
+        sleep(Duration::from_millis(opts.period));
         let mut maps = skel.maps();
         let dist = maps.dist();
         let key = Key { value: 1 };
@@ -54,7 +75,26 @@ fn main() -> Result<()> {
             let mut value_ = Value::default();
             plain::copy_from_bytes(&mut value_, &value).expect("invalid value bytes");
 
-            println!("Got: {:?}", value_);
+            let x = (countup * opts.period) as f32 / 1000f32;
+            let y = if opts.show_count {
+                value_.count as f32
+            } else if opts.show_avg {
+                if value_.count == 0 {
+                    0.0f32
+                } else {
+                    value_.processed_bytes as f32 / value_.count as f32
+                }
+            } else {
+                value_.processed_bytes as f32
+            };
+            points.push((x, y));
+        }
+        print!(".");
+        let mut stdout = BufWriter::new(std::io::stdout());
+        stdout.flush()?;
+        countup += 1;
+        if countup % 60 == 0 {
+            print!("\n");
         }
 
         let value = Value {
@@ -66,6 +106,11 @@ fn main() -> Result<()> {
         dist.update(key_, value_, MapFlags::empty())?
     }
     println!("");
+
+    let xmax = ((countup + 1) * opts.period) as f32 / 1000f32;
+    Chart::new(200, 60, 0.0, xmax)
+        .lineplot(&Shape::Steps(&points))
+        .display();
 
     Ok(())
 }
